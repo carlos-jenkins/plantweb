daa90f424f50db48bfbb35c32748da705bc00c53
Carlos Jenkins <carlos@jenkins.co.cr>
2016-06-26 04:22:34 -0600

    new: dev: Added test to check directive when called with arguments.

--- test/test_plantweb_directive.py
+++ test/test_plantweb_directive.py
@@ -25,7 +25,7 @@ from __future__ import unicode_literals, absolute_import
 from __future__ import print_function, division
 
 from os import listdir
-from os.path import join
+from os.path import join, basename
 
 
 def identify_content(content):
@@ -36,6 +36,13 @@ def identify_content(content):
     if content.startswith('@startditaa'):
         return 'diagram'
     return None
+
+
+def images_in(path):
+    return [
+        f for f in listdir(path)
+        if f.endswith('.png') or f.endswith('.svg')
+    ]
 
 
 DIRECTIVE_CONTENT_TPL = """
@@ -49,6 +56,7 @@ def test_directive_content(sources, sphinx):
 
     directives = []
 
+    # Read source files and format directives
     for src in sources:
         with open(src, 'rb') as fd:
             content = fd.read().decode('utf-8')
@@ -62,19 +70,42 @@ def test_directive_content(sources, sphinx):
             )
             directives.append(directive)
 
+    # Execute sphinx build
     sphinx('\n'.join(directives))
 
-    def images_in(path):
-        return [
-            f for f in listdir(path)
-            if f.endswith('.png') or f.endswith('.svg')
-        ]
-
+    # Assert images creation
     cached_images = images_in(sphinx.cachedir)
     copied_images = images_in(join(sphinx.outdir, '_images'))
     assert len(sources) == len(cached_images)
     assert sorted(cached_images) == sorted(copied_images)
 
 
+DIRECTIVE_ARGUMENT_TPL = """
+.. {}:: {}
+
+"""
+
+
 def test_directive_argument(sources, sphinx):
-    pass
+    directives = []
+
+    # Read sources and create argument files
+    for src in sources:
+        with open(src, 'rb') as fdin:
+            content = fdin.read().decode('utf-8')
+
+            # Write destination file
+            srcfile = basename(src)
+
+            with open(join(sphinx.srcdir, srcfile), 'w') as fdout:
+                fdout.write('\n'.join(content.split('\n')[1:-2]))
+
+            # Format and add directive
+            directive_name = identify_content(content)
+            directive = DIRECTIVE_ARGUMENT_TPL.format(
+                directive_name, srcfile
+            )
+            directives.append(directive)
+
+    # Execute sphinx build
+    sphinx('\n'.join(directives))
