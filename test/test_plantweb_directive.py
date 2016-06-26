ed894681fe2fb2a36ce7f13e67fdc8629e6f07a4
Carlos Jenkins <carlos@jenkins.co.cr>
2016-06-26 04:09:44 -0600

    fix: dev: Added asserts to the directive content test.

--- test/test_plantweb_directive.py
+++ test/test_plantweb_directive.py
@@ -24,8 +24,11 @@ See http://pythontesting.net/framework/pytest/pytest-introduction/#fixtures
 from __future__ import unicode_literals, absolute_import
 from __future__ import print_function, division
 
+from os import listdir
+from os.path import join
 
-def identify_and_strip_tags(content):
+
+def identify_content(content):
     if content.startswith('@startuml'):
         return 'uml'
     if content.startswith('@startdot'):
@@ -50,7 +53,7 @@ def test_directive_content(sources, sphinx):
         with open(src, 'rb') as fd:
             content = fd.read().decode('utf-8')
 
-            directive_name = identify_and_strip_tags(content)
+            directive_name = identify_content(content)
             directive = DIRECTIVE_CONTENT_TPL.format(
                 directive_name,
                 '\n    '.join(
@@ -60,6 +63,17 @@ def test_directive_content(sources, sphinx):
             directives.append(directive)
 
     sphinx('\n'.join(directives))
+
+    def images_in(path):
+        return [
+            f for f in listdir(path)
+            if f.endswith('.png') or f.endswith('.svg')
+        ]
+
+    cached_images = images_in(sphinx.cachedir)
+    copied_images = images_in(join(sphinx.outdir, '_images'))
+    assert len(sources) == len(cached_images)
+    assert sorted(cached_images) == sorted(copied_images)
 
 
 def test_directive_argument(sources, sphinx):
